da3a8a1b4d969e3528bf9741cee1bd931f403e3c
Huy Truong <huy.truong@mevry.com>
2020-07-15 23:46:55 +0700

    Updates

---
+++ precache-manifest.aef63d99f8b9b5a749fa8527b3e59030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105d7f9d0d18696a1f14d3e809263208",
+    "url": "/Landing-Pages/index.html"
+  },
+  {
+    "revision": "44697c20f72fdb5a9263",
+    "url": "/Landing-Pages/static/css/main.6321b756.chunk.css"
+  },
+  {
+    "revision": "51394b9879d3730ae67f",
+    "url": "/Landing-Pages/static/js/2.5ca03385.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Landing-Pages/static/js/2.5ca03385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44697c20f72fdb5a9263",
+    "url": "/Landing-Pages/static/js/main.685ade8f.chunk.js"
+  },
+  {
+    "revision": "881662071002872acb5f",
+    "url": "/Landing-Pages/static/js/runtime-main.b091a948.js"
+  },
+  {
+    "revision": "8dbe480da3a89d35e622d7e4b48eed4b",
+    "url": "/Landing-Pages/static/media/blue-trading.8dbe480d.jpg"
+  }
+]);
