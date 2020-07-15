6db76096a3b1e197bb774d439d087a339472f14e
Huy Truong <huy.truong@mevry.com>
2020-07-15 15:44:15 +0700

    prepare for coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './App.scss';
+import { Link } from 'react-router-dom';
+import BlueTradingImg from 'images/blue-trading.jpg';
 
 function App() {
   return (
-    <div className="App">
-        <div>
-          Card
+    <div className="landing-pages">
+      {[...Array(4)].map((item, index) => (
+        <div className="landing-pages__item" key={index}>
+          <div className="landing-pages__item__wrapper">
+            <Link to="/bluetrading">
+              <img src={BlueTradingImg} alt="blue-trading" />
+            </Link>
+            <div>Trading XD landing page template</div>
+          </div>
         </div>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from 'App';
+import BlueTrading from 'pages/BlueTrading';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
@@ -9,7 +10,7 @@ const Root = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/landingpage1" component={App} />
+      <Route exact path="/bluetrading" component={BlueTrading} />
     </Switch>
   </Router>
 )
